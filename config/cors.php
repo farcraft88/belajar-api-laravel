7fd9c5182127e2228bd879c5a2e3b6e07c907d38
farcraft88 <farcraft88@gmail.com>
2025-12-01 20:59:40 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,41 @@
 <?php
+
 return [
-    'paths' => ['api/*', 'health'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' =>
-    ['http://localhost:5173', 'http://127.0.0.1:5173'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This configuration controls your application's handling of Cross-Origin
+    | Resource Sharing (CORS). This determines which cross-origin operations
+    | may be executed in web browsers. You are free to adjust these settings
+    | as needed for your application.
+    |
+    */
+
+    'paths' => [
+        'api/*',              // izinkan semua endpoint API
+        'login',
+        'logout',
+        'sanctum/csrf-cookie'
+    ],
+
+    'allowed_methods' => ['*'], // semua metode: GET, POST, PUT, PATCH, DELETE
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
+
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+
+    'allowed_headers' => ['*'], // semua header boleh
+
     'exposed_headers' => [],
-    'max_age' => 3600,
+
+    'max_age' => 0,
+
+    // Jika tidak pakai cookie (karena Sanctum token mode), maka false
     'supports_credentials' => false,
 ];
